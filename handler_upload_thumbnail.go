0fbb698954ca2d2765526f31e5dc2f720dca8898
tobi <tobi19.py@gmail.com>
2025-06-09 10:14:22 -0500

    Get and update thumbnail url dynamically

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
+	type response struct {
+		database.Video
+	}
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -28,10 +33,44 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	fileType := fileHeader.Header.Get("Content-Type")
+	fileData, err := io.ReadAll(file)
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to get video", err)
+		return
+	}
+	if dbVideo.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized to get video data", err)
+		return
+	}
+
+	rThumbnail := thumbnail{
+		data:      fileData,
+		mediaType: fileType,
+	}
+
+	videoThumbnails[dbVideo.ID] = rThumbnail
+
+	newURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%s", cfg.port, videoIDString)
+	dbVideo.ThumbnailURL = &newURL
+	err = cfg.db.UpdateVideo(dbVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, response{database.Video{}})
 }
