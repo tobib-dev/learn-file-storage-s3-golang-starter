9a5803d31e657ee55c1d1a533b9d7fb9062f0a83
tobi <tobi19.py@gmail.com>
2025-06-10 09:23:36 -0500

    Refactor cache to no-store

--- app/app.js
+++ app/app.js
@@ -266,7 +266,7 @@ function viewVideo(video) {
     thumbnailImg.style.display = "none";
   } else {
     thumbnailImg.style.display = "block";
-    thumbnailImg.src = `${video.thumbnail_url}?v=${Date.now()}`;
+    thumbnailImg.src = video.thumbnail_url;
   }
 
   const videoPlayer = document.getElementById("video-player");
